7e65c9b1336e72060d79085f2f61eb9b151e07ea
Shashank Jaiswal <jaisw7@gmail.com>
2021-05-08 17:41:15 -0400

    Add supplementary files

--- Section 4.1/RawData/bkwbi_vss_cufft.py
+++ Section 4.1/RawData/bkwbi_vss_cufft.py
@@ -19,7 +19,7 @@ Refer to the PhDThesis to understand the meaning of these parameters.
 
 The inputs are purely meant for benchmarking. You can write a simple bash 
 loop to test the effect of input on the accuracy. To run the script, 
-you will need to install the following Python modules installed on your system:
+you will need the following Python modules installed on your system:
 - mako
 - pycuda
 - numpy
